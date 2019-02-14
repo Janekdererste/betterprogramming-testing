165aa2acfc64fdbe8d4ed04ade16b3acc1c5667e
Janek Laudan <laudan@tu-berlin.de>
2019-02-14 08:55:27 +0100

    Remove code to create sceleton project for the seminar

--- src/main/java/com/janeklaudan/betterProgramming/firstExample/AbstractEntity.java
+++ src/main/java/com/janeklaudan/betterProgramming/firstExample/AbstractEntity.java
@@ -1,8 +1,6 @@
 package com.janeklaudan.betterProgramming.firstExample;
 
 
-import org.apache.commons.lang3.StringUtils;
-
 public class AbstractEntity {
 
     private String id;
@@ -13,19 +11,5 @@ public class AbstractEntity {
 
     public void setId(String id) {
         this.id = id;
-    }
-
-    @Override
-    public boolean equals(Object other) {
-
-        if (this == other)
-            return true;
-
-        if (other instanceof AbstractEntity) {
-            AbstractEntity otherEntity = (AbstractEntity) other;
-            if (StringUtils.isNotEmpty(id) && StringUtils.isNotEmpty(((AbstractEntity) other).getId()))
-                return id.equals(otherEntity.getId());
-        }
-        return false;
     }
 }
